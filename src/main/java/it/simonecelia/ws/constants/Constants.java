2d2cad011c5d916a06027567d1d22ea093362b16
Simone Celia <simon387@hotmail.it>
2020-07-01 21:39:18 +0200

    added constants

--- src/main/java/it/simonecelia/ws/constants/Constants.java
+++ src/main/java/it/simonecelia/ws/constants/Constants.java
@@ -1,4 +1,8 @@
 package it.simonecelia.ws.constants;
 
-public final class Constants {
+public abstract class Constants {
+	public static final String NAME = "name";
+	public static final String PERSON = "person";
+	public static final String ID = "id";
+	public static final String PHONE = "phone";
 }
