ea2d3ea294ff3bf0381b344c0fadd6458efb83f3
simone celia <simon387@hotmail.it>
2020-07-01 16:23:46 +0200

    switch_to_gradle

--- src/main/java/it/simonecelia/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package it.simonecelia.repository;
-
-import it.simonecelia.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
